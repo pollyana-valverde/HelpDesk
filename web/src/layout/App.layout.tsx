edc6a9559d94f9a8c5a2bab1dfe25d561b264e70
Pollyana Valverde <valverdepolly@gmail.com>
2026-01-11 12:26:13 -0300

    fix(layout): remove unnecessary responsive class from AppLayout component

--- web/src/layout/App.layout.tsx
+++ web/src/layout/App.layout.tsx
@@ -6,7 +6,7 @@ export function AppLayout() {
   return (
     <main className="w-screen h-screen bg-gray-900 md:pt-8 md:flex ">
       <Navigation />
-      <div className="bg-gray-100 h-full flex-1 sm:rounded-t-3xl md:rounded-tr-none py-7 px-6 md:px-12 md:py-13">
+      <div className="bg-gray-100 h-full flex-1 rounded-t-3xl md:rounded-tr-none py-7 px-6 md:px-12 md:py-13">
         <Outlet />
       </div>
     </main>
