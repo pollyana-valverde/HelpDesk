8235526c1bcecdaf04fc62524258b7d1abdd8dc4
Pollyana Valverde <valverdepolly@gmail.com>
2026-01-11 15:17:08 -0300

    fix(layout): remove unnecessary responsive class from AuthLayout component

--- web/src/layout/Auth.layout.tsx
+++ web/src/layout/Auth.layout.tsx
@@ -9,7 +9,7 @@ export function AuthLayout() {
       className="w-screen h-screen bg-cover bg-center pt-8 flex justify-end"
       style={{ backgroundImage: `url(${loginBg})` }}
     >
-      <main className="h-full w-full sm:rounded-t-3xl md:max-w-1/2 md:rounded-tr-none bg-gray-100 py-8 px-6 flex flex-col items-center">
+      <main className="h-full w-full rounded-t-3xl md:max-w-1/2 md:rounded-tr-none bg-gray-100 py-8 px-6 flex flex-col items-center">
         <header className="flex gap-3 mb-6 items-center md:mb-8">
           <img src={logoDark} alt="Logo Dark" className="h-10 w-10" />
           <h1 className="text-2xl text-indigo-800 font-bold">HelpDesk</h1>
