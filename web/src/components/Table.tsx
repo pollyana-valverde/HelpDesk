fce3b7ed4b62f8c630ffc97bc3302726ed974be8
Pollyana Valverde <valverdepolly@gmail.com>
2026-01-13 20:37:13 -0300

    style(table): add hover effect to TableRow for improved user interaction

--- web/src/components/Table.tsx
+++ web/src/components/Table.tsx
@@ -33,7 +33,7 @@ function TableBody({ children, ...rest }: React.ComponentProps<"tbody">) {
 
 function TableRow({ children, ...rest }: React.ComponentProps<"tr">) {
   return (
-    <tr className="border-t border-gray-300 text-gray-800 h-16" {...rest}>
+    <tr className="border-t border-gray-300 text-gray-800 h-16 hover:bg-gray-200 transition ease-linear" {...rest}>
       {children}
     </tr>
   );
