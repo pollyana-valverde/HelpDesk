add0bc584ff5f3af9c1242dcd563193f71cf17f2
Pollyana Valverde <valverdepolly@gmail.com>
2026-01-15 23:20:54 -0300

    fix(TableRoot): correct overflow property for better table scrolling experience

--- web/src/components/Table/Root.tsx
+++ web/src/components/Table/Root.tsx
@@ -9,7 +9,7 @@ export function TableRoot({ children, ...rest }: React.ComponentProps<"table">) 
 //   );
 
   return (
-    <div className="w-full overflow-scroll rounded-xl border border-gray-300">
+    <div className="w-full overflow-x-auto rounded-xl border border-gray-300">
       <table className="w-full table-fixed md:table-auto text-left" {...rest}>
         {/* {head} */}
         {children}
