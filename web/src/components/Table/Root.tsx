f19c9de967c2a998db6c9be94e0963120efd4a4e
Pollyana Valverde <valverdepolly@gmail.com>
2026-01-15 23:22:03 -0300

    refactor(TableRoot): remove unused code for table head and body mapping

--- web/src/components/Table/Root.tsx
+++ web/src/components/Table/Root.tsx
@@ -1,17 +1,9 @@
 import React from "react";
 
 export function TableRoot({ children, ...rest }: React.ComponentProps<"table">) {
-//   const head = React.Children.map(children, (child) =>
-//     (child as React.ReactElement).type === TableHead ? child : null
-//   );
-//   const body = React.Children.map(children, (child) =>
-//     (child as React.ReactElement).type === TableBody ? child : null
-//   );
-
   return (
     <div className="w-full overflow-x-auto rounded-xl border border-gray-300">
       <table className="w-full table-fixed md:table-auto text-left" {...rest}>
-        {/* {head} */}
         {children}
       </table>
     </div>
